c7499e8c677368c8fecbcee6268224edaf99f06b
Patrick Navarre <navarre@uchicago.edu>
2024-03-23 14:57:09 -0500

    updated backend

---
+++ backend/json_boilerplate.py
@@ -0,0 +1,41 @@
+import json
+import requests
+import os
+from openai import OpenAI
+
+
+def json_converter(prompt: str) -> json:
+    prompt = prompt + " Return the response in json."
+    data = {'prompt': prompt, 
+            'response': 'This is a response based on the prompt'}
+    
+    with open('output.json', 'w') as json_file:
+        json.dump(data, json_file, indent=4)
+    
+
+    client = OpenAI(
+        # This is the default and can be omitted
+        api_key=os.environ.get("OPENAI_API_KEY")
+    )
+
+
+
+    # prompt = f"Given the following movie description: \"{json_file}\", provide the genre, target audience, and suggested rating."
+
+    print("got here")
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": prompt,
+            }
+        ],
+        model="gpt-3.5-turbo",
+        response_format={"type": "json_object"}
+    )
+    print("Chat completion: ", chat_completion) 
+    return json.loads(chat_completion.choices[0].message.content)['response']
+
+
+# res = json_converter("Say something funny please! Return the response in json.")
+# print(res)
