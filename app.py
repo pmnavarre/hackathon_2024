dbb848e27078b9b1f9a7e766be0857f896128754
Drew Vlasnik <drewvlaz@uchicago.edu>
2024-03-23 17:39:17 -0500

    add ui improvements

--- app.py
+++ app.py
@@ -1,16 +1,96 @@
 import streamlit as st
 import backend.tmdb
-import time
 from backend.json_boilerplate import json_converter
 
-# Title of the main page
-st.set_page_config(layout="wide")
-st.title("Movie Recommendation System")
+MOVIE_PARAMS = {
+    "include_adult": "false",
+    "include_video": "false",
+    "language": "en-US",
+    "page": "1",
+    "sort_by": "popularity.desc",
+    "watch_region": "US",
+}
+
+WATCH_PROVIDERS = {
+    "Netflix": 8,
+    "Amazon Prime Video": 119,
+    "Disney+": 337,
+    "HBO Max": 384,
+    "Apple TV": 2,
+    "YouTube": 192,
+}
 
 
-images = [
-    "https://image.tmdb.org/t/p/w200/iuFNMS8U5cb6xfzi51Dbkovj7vM.jpg" for _ in range(10)
-]
+def sidebar():
+    st.sidebar.title("Filters")
+    year_opts = ["Any"] + [str(i) for i in range(2024, 1969, -1)]
+    year = st.sidebar.selectbox("Year", year_opts)
+    min_runtime = st.sidebar.selectbox(
+        "Minimum runtime", ["Any", "90 min", "120 min", "150 min"]
+    )
+    max_runtime = st.sidebar.selectbox(
+        "Maximum runtime", ["Any", "90 min", "120 min", "150 min"]
+    )
+    MOVIE_PARAMS["vote_average.gte"] = st.sidebar.slider(
+        "Minimum rating", 0.0, 10.0, 7.5
+    )
+    with_watch_providers = st.sidebar.multiselect(
+        "Streaming Platforms",
+        sorted(["Netflix", "Amazon Video", "Disney+", "Hulu", "Apple TV"]),
+    )
+
+    if year != "Any":
+        MOVIE_PARAMS["primary_release_year"] = str(year)
+    if min_runtime != "Any":
+        MOVIE_PARAMS["with_runtime.gte"] = str(min_runtime)
+    if max_runtime != "Any":
+        MOVIE_PARAMS["with_runtime.lte"] = str(max_runtime)
+    if with_watch_providers:
+        MOVIE_PARAMS["with_watch_providers"] = "|".join(
+            [str(WATCH_PROVIDERS[wp]) for wp in with_watch_providers]
+        )
+
+
+def validate_params():
+    if "with_runtime.gte" in MOVIE_PARAMS and "with_runtime.lte" in MOVIE_PARAMS:
+        if int(MOVIE_PARAMS["with_runtime.gte"]) > int(
+            MOVIE_PARAMS["with_runtime.lte"]
+        ):
+            st.error("Minimum runtime must be less than maximum runtime")
+            return False
+    return True
+
+
+def run():
+    st.set_page_config(layout="wide")
+    st.title("Movie Recommendation System")
+    # page = st.session_state.get("page", "Main Page")
+
+    sidebar()
+
+    st.header("Discover something new!")
+    with st.form(key="my_form"):
+        input_prompt = st.text_input(
+            "Provide a description of what you want to watch:", ""
+        )
+        submit_button = st.form_submit_button("Submit")
+
+    if submit_button and validate_params():
+        st.write(
+            f"Generating keywords for movie recommendations based on: {input_prompt}"
+        )
+        movie_params = json_converter(input_prompt)
+        st.write(movie_params)
+
+        tmdb = backend.tmdb.TMDB(st.secrets["TMDB_API_KEY"])
+        # movie_params["primary_release_year"] = input_prompt.split(" ")[0]
+
+        # with st.spinner("Finding movies..."):
+        movies = tmdb.discover_movies(MOVIE_PARAMS, num_movies=50)
+        # movies = tmdb.discover_movies(movie_params, num_movies=50)
+        display_movies(movies, cols=6)
+        st.toast("We found some movies for you!", icon="😍")
+        # st.write(f"You submitted: {input_prompt}")
 
 
 def display_movies(movies, cols=4, width=150):
@@ -20,59 +100,13 @@ def display_movies(movies, cols=4, width=150):
                 image_domain = "https://image.tmdb.org/t/p/w200"
                 image_url = f"{image_domain}/{movie['poster_path']}"
                 st.image(image_url, width=width, caption=movie["title"])
-            if col.button(
-                "![Image]",
-                key="-".join(movie["title"]),
-                help="Click to show info for Image 2",
-            ):
-                pass
+            # if col.button(
+            #     "![Image]",
+            #     key="-".join(movie["title"]),
+            #     help="Click to show info for Image 2",
+            # ):
+            #     pass
 
 
-# # Sidebar for navigation
-# with st.sidebar:
-#     st.title("Navigation")
-#     if st.button("Main Page"):
-#         st.session_state["page"] = "Main Page"
-#     if st.button("Other Page"):
-#         st.session_state["page"] = "Other Page"
-
-page = st.session_state.get("page", "Main Page")
-
-movie_params = {
-    "include_adult": "false",
-    "include_video": "false",
-    "language": "en-US",
-    "page": "1",
-    "sort_by": "popularity.desc",
-}
-
-
-# def char_generator(input_str):
-#     """A generator function that yields each character of the input string."""
-#     for char in input_str:
-#         yield char
-#         time.sleep(0.1)
-#
-#
-
-if page == "Main Page":
-    st.header("Welcome to the Main Page")
-    # st.write_stream(char_generator("Discover something new!"))
-    with st.form(key="my_form"):
-        input_prompt = st.text_input("Enter something:", "")
-        submit_button = st.form_submit_button("Submit")
-
-    if submit_button:
-        st.write(f"Generating keywords for movie recommendations based on: {input_prompt}")
-        movie_params = json_converter(input_prompt)
-        tmdb = backend.tmdb.TMDB(st.secrets["TMDB_API_KEY"])
-        st.write(movie_params)
-        # movie_params["primary_release_year"] = input_prompt.split(" ")[0]
-        movies = tmdb.discover_movies(movie_params)
-        display_movies(movies["results"], cols=6)
-        st.write(f"You submitted: {input_prompt}")
-
-elif page == "Other Page":
-    st.write(
-        "This is another page. You can add different widgets or functionalities here."
-    )
+if __name__ == "__main__":
+    run()
--- backend/tmdb.py
+++ backend/tmdb.py
@@ -1,19 +1,64 @@
 import requests
-import os
 import json
+import streamlit as st
+
+PAGE_SIZE = 20
 
 
 class TMDB:
     def __init__(self, api_key):
         self.api_key = api_key
-
-    def discover_movies(self, params):
-        url = "https://api.themoviedb.org/3/discover/movie"
-        headers = {
+        self.headers = {
             "Authorization": f"Bearer {self.api_key}",
             "accept": "application/json",
         }
-        response = requests.get(url, headers=headers, params=params)
+
+    def discover_movies(self, params, num_movies=PAGE_SIZE):
+        url = "https://api.themoviedb.org/3/discover/movie"
+        response = []
+        for i in range(num_movies // PAGE_SIZE):
+            params["page"] = i + 1
+            response += requests.get(url, headers=self.headers, params=params).json()[
+                "results"
+            ]
+        return response
+
+    def get_popular_movies(self):
+        url = "https://api.themoviedb.org/3/movie/popular"
+        params = {"language": "en-US", "page": "1"}
+        response = requests.get(url, headers=self.headers, params=params)
+        return response.json()
+
+    def add_to_watchlist(self, media_id, account_id=21124543):
+        url = f"https://api.themoviedb.org/3/account/{account_id}/watchlist"
+        payload = {"media_type": "movie", "media_id": media_id, "watchlist": True}
+        response = requests.post(url, headers=self.headers, json=payload)
+        return response.json()
+
+    def get_watchlist(self, account_id=21124543):
+        url = f"https://api.themoviedb.org/3/account/{account_id}/watchlist/movies"
+        params = {
+            "language": "en-US",
+            "page": "1",
+            "sort_by": "created_at.asc",
+        }
+        response = requests.get(url, headers=self.headers, params=params)
+        return response.json()
+
+    def get_request_token(self):
+        url = "https://api.themoviedb.org/3/authentication/token/new"
+        response = requests.get(url, headers=self.headers)
+        return response.json()
+
+    def get_movie_reviews(self, movie_id):
+        url = f"https://api.themoviedb.org/3/movie/{movie_id}/reviews?language=en-US&page=1"
+        params = {"language": "en-US", "page": "1"}
+        response = requests.get(url, headers=self.headers, params=params)
+        return response.json()
+
+    def get_watch_providers(self, movie_id):
+        url = f"https://api.themoviedb.org/3/movie/{movie_id}/watch/providers"
+        response = requests.get(url, headers=self.headers)
         return response.json()
 
 
@@ -27,15 +72,27 @@ if __name__ == "__main__":
         "primary_release_year": "2023",
     }
 
-    tmdb = TMDB(os.environ.get("TMDB_API_KEY"))
-    res = tmdb.discover_movies(params)
-    print(json.dumps(res, indent=4))
-    print(res["results"][0].keys())
-    print(len(res["results"]))
-    print(res.keys())
-
-    for movie in res["results"]:
-        print(movie["title"])
-        print(
-            movie["poster_path"]
-        )  # https://image.tmdb.org/t/p/w200/iuFNMS8U5cb6xfzi51Dbkovj7vM.jpg
+    tmdb = TMDB(st.secrets["TMDB_API_KEY"])
+    # res = tmdb.discover_movies(params)
+    # print(json.dumps(res, indent=4))
+    # print(res["results"][0].keys())
+    # print(len(res["results"]))
+    # print(res.keys())
+    #
+    # for movie in res["results"]:
+    #     print(movie["title"])
+    #     print(
+    #         movie["poster_path"]
+    #     )  # https://image.tmdb.org/t/p/w200/iuFNMS8U5cb6xfzi51Dbkovj7vM.jpg
+    #     print(movie["vote_average"])
+    #
+    # # response = tmdb.add_to_watchlist(res["results"][0]["id"])
+    # # print(response)
+    # print(tmdb.get_watchlist())
+    movie = tmdb.get_popular_movies()["results"][1]
+    res = tmdb.get_watch_providers(19)["results"]["US"]
+    types = ["rent", "buy", "flatrate"]
+    for t in types:
+        for provider in res[t]:
+            print(provider["provider_name"], provider["provider_id"])
+    # print(json.dumps(res, indent=4))
