10e8a4d64bc46f609db5c9bd6bed5a8a6b64dd4b
Drew Vlasnik <drewvlaz@uchicago.edu>
2024-03-24 00:16:50 -0500

    add login

--- backend/json_boilerplate.py
+++ backend/json_boilerplate.py
@@ -13,8 +13,8 @@ def invoke_openai(prompt: str) -> Union[dict, None]:
                 "content": prompt,
             }
         ],
-        model="gpt-3.5-turbo",
-        # model="gpt-4-0125-preview",
+        # model="gpt-3.5-turbo",
+        model="gpt-4-0125-preview",
         response_format={"type": "json_object"},
     )
 
@@ -40,7 +40,7 @@ def json_converter(input: str, genres: list) -> Union[dict, None]:
 
     Please make sure you complete the objective above with the following rules:
     1/ For genres, return the relevant genres from the list {genres}. They must be comma separated and capitalized.
-    2/ For keywords, this should be a short list of words describing the overall mood. 
+    2/ For keywords, this should be 1 or 2 words to capture the uniqueness of the prompt. Return an empty list if there are no relevant keywords.
     3/ For actors, only return the name of an actor if it specifically mentioned in the prompt. If the prompt requires all of the actors to be in the same movie, set the all_actors flag to True. Otherwise, if the prompt does not require all of the actors to be in the same movie, set the all_actors flag to False.
 
     If you are unsure about any category based on the prompt, return None for that category. The prompt is:\n\n{input}"""
--- pages/discover.py
+++ pages/discover.py
@@ -1,5 +1,8 @@
 import json
+import hmac
+
 import streamlit as st
+
 from backend.tmdb import TMDB
 from backend.json_boilerplate import json_converter, extract_keywords
 from backend.utils import display_movies
@@ -147,5 +150,26 @@ def run():
                 st.write(MOVIE_PARAMS)
 
 
-if __name__ == "__main__":
-    run()
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the password.
+        else:
+            st.session_state["password_correct"] = False
+
+    if st.session_state.get("password_correct", False):
+        return True
+
+    st.text_input(
+        "Password", type="password", on_change=password_entered, key="password"
+    )
+    if "password_correct" in st.session_state:
+        st.error("😕 Password incorrect")
+    return False
+
+
+run()
